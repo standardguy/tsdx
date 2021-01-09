4ffcf78c008dcf57c9067ec1679a39e3d2b338cd
Robert Gonia <robertgonia@Roberts-MBP.fios-router.home>
2021-01-09 01:23:21 -0500

    Updates

--- insticatorsession.esm.js
+++ insticatorsession.esm.js
@@ -1,7 +1,7 @@
 var sum = function sum(a, b) {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop boop');
-  }
+  // if ('development' === process.env.NODE_ENV) {
+  //   console.log('boop boop');
+  // }
 
   return a + b;
 };
